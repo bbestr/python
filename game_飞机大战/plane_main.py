e5006d76ab92762c054c91b0414af0026f2034ab
best <1692065699@qq.com>
2022-10-15 00:20:22 +0800

    飞机大战 2

--- game_飞机大战/plane_main.py
+++ game_飞机大战/plane_main.py
@@ -1,49 +1,47 @@
 import pygame
 from plane_sprite import *
 
-FRAME_SEC = 60
-CREATE_ENEMY = pygame.USEREVENT
-SCREEN_R = pygame.Rect(0,0,480,700)
-ENEMY_MODEL = pygame.Rect(0,0,53,43)
 class PlaneGame(object):
     def __init__(self):
         print("游戏初始化")
 
-        #创建游戏窗口
-        self.screen = pygame.display.set_mode((SCREEN_R.width,SCREEN_R.height))
+        # 创建游戏窗口
+        self.screen = pygame.display.set_mode((SCREEN_R.width, SCREEN_R.height))
 
-        #创建游戏时钟
+        # 创建游戏时钟
         self.clock = pygame.time.Clock()
 
-
-        #创建精灵组
+        # 创建精灵组
         self.__create_sprites()
 
+        self.hero = Hero()
 
-        #创建定时器 创建敌人
-        pygame.time.set_timer(CREATE_ENEMY,1000)
+        self.hero_group = pygame.sprite.Group(self.hero)
+        # 创建定时器 创建敌人
+        pygame.time.set_timer(CREATE_ENEMY, 1000)
+
     def __create_sprites(self):
         """创建精灵"""
         bg1 = background(False)
         bg2 = background()
-        self.background = pygame.sprite.Group(bg1,bg2)
+        self.background = pygame.sprite.Group(bg1, bg2)
 
         self.enemy_group = pygame.sprite.Group()
 
     def start_game(self):
         print("游戏开始")
         while True:
-            #设置刷新率
+            # 设置刷新率
             self.clock.tick(FRAME_SEC)
-            #事件监听
+            # 事件监听
             self.__event_hander()
-            #碰撞检测
+            # 碰撞检测
             self.__check_collide()
-            #更新绘制 精灵
+            # 更新绘制 精灵
             self.__updatesprite()
-            #更新显示
+            # 更新显示
             pygame.display.update()
-            pass
+
     def __event_hander(self):
         """时间解控"""
         for even in pygame.event.get():
@@ -55,6 +53,14 @@ class PlaneGame(object):
                 enemy1 = enmey_create()
                 self.enemy_group.add(enemy1)
 
+        keyspress = pygame.key.get_pressed()
+
+        if keyspress[pygame.K_LEFT]:
+            self.hero.speed = -2
+        elif keyspress[pygame.K_RIGHT]:
+            self.hero.speed = 2
+        else:
+            self.hero.speed = 0
 
     def __check_collide(self):
         """碰撞检测"""
@@ -66,6 +72,9 @@ class PlaneGame(object):
 
         self.enemy_group.update()
         self.enemy_group.draw(self.screen)
+
+        self.hero_group.update()
+        self.hero_group.draw(self.screen)
 
     @staticmethod
     def __gameover():
--- game_飞机大战/plane_sprite.py
+++ game_飞机大战/plane_sprite.py
@@ -1,38 +1,64 @@
 import pygame
 import random
-from plane_main import *
+
+FRAME_SEC = 60
+CREATE_ENEMY = pygame.USEREVENT
+SCREEN_R = pygame.Rect(0, 0, 480, 700)
+ENEMY_MODEL = pygame.Rect(0, 0, 53, 43)
 
 class GameSprite(pygame.sprite.Sprite):
     """飞机大战 飞机精灵"""
-    def __init__(self,image_name,speed=1):
+
+    def __init__(self, image_name, speed=1):
         super().__init__()
         self.image = pygame.image.load(image_name)
         self.speed = speed
         self.rect = self.image.get_rect()
+
     def update(self):
         self.rect.y += self.speed
+
+
 class background(GameSprite):
-     """"游戏背景"""
-     def __init__(self,alt = True):
-         super().__init__("./images/background.png")
-         self.alt = alt
-         if self.alt:
-             self.rect.y = -SCREEN_R.height
-     def update(self):
-        #1.调用父类
+    """"游戏背景"""
+
+    def __init__(self, alt=True):
+        super().__init__("./images/background.png")
+        self.alt = alt
+        if self.alt:
+            self.rect.y = -SCREEN_R.height
+
+    def update(self):
+        # 1.调用父类
         super().update()
-        #2. 判断背景
+        # 2. 判断背景
         if self.rect.y >= SCREEN_R.height:
             self.rect.y = -SCREEN_R.height
+
+
 class enmey_create(GameSprite):
     def __init__(self):
         super().__init__("./images/enemy1.png")
 
-        self.speed = random.randint(1,3)
+        self.speed = random.randint(1, 3)
         self.rect.y = -ENEMY_MODEL.height
-        self.rect.x = random.randint(0,SCREEN_R.width -ENEMY_MODEL.width)
+        self.rect.x = random.randint(0, SCREEN_R.width - ENEMY_MODEL.width)
+
     def update(self):
         super().update()
         if self.rect.y >= 700:
             self.kill()
 
+
+class Hero(GameSprite):
+    def __init__(self):
+        super().__init__("./images/me1.png", 0)
+        self.rect.centerx = SCREEN_R.centerx
+        self.rect.bottom = SCREEN_R.height - 120
+
+    def update(self):
+        self.rect.x += self.speed
+        if self.rect.x <0:
+            self.rect.x = 0
+        elif self.rect.right > SCREEN_R.width:
+            self.rect.right = SCREEN_R.width
